1eba66306161a4c4fbffb70c3acafa8a4430bca3
GouruvSangar <1006102@student.sae.edu.au>
2019-11-08 19:55:35 +1100

    Weapon face mouse script (not working :C)

---
+++ Side-Scroller-Puzzle-Game/Assets/Characters/Player/Code/Weapons/FaceMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMouse : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x = transform.position.x,
+            mousePosition.y = transform.position.y
+        );
+
+        transform.up = direction;
+    }
+}
